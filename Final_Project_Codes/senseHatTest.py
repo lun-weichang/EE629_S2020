67600331a0e5fdfbd263067f12698a092c60aa7b
lun-weichang <lunweichang539@gmail.com>
2020-04-27 16:05:57 -0400

    Modified GDOCS_OAUTH_JSON variable

--- Final_Project_Codes/senseHatTest.py
+++ Final_Project_Codes/senseHatTest.py
@@ -10,7 +10,7 @@ import subprocess
 from system_info import get_temperature
 from oauth2client.service_account import ServiceAccountCredentials
 
-GDOCS_OAUTH_JSON       = 'fit-aleph-272014-31af88a26a13.json'
+GDOCS_OAUTH_JSON       = 'ee629wsproject-7d249e8f524e.json'
 GDOCS_SPREADSHEET_NAME = 'WS_Data_Sheet'
 FREQUENCY_SECONDS      = 10
 
